80325a46692a17dc07b9d780dd51fbcac11a0a36
Jared Clemmensen and Kimberly Huynh <jared@>
2018-01-10 11:45:30 -0800

    add modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
 
-  $("#appt").submit(function(event) {
-    event.preventDefault();
-    var name = $("input#name").val();
-    var description = $("input#description").val();
-    var date = $("input#date").val();
-    var time = $("input#time").val();
-
-    $("span#name").text(name);
-    $("span#description").text(description);
-    $("span#date").text(date);
-    $("span#time").text(time);
+  // $("#appt").submit(function(event) {
+  //   event.preventDefault();
+  //   var name = $("input#name").val();
+  //   var description = $("input#description").val();
+  //   var date = $("input#date").val();
+  //   var time = $("input#time").val();
+  //
+  //   $("span#name").text(name);
+  //   $("span#description").text(description);
+  //   $("span#date").text(date);
+  //   $("span#time").text(time);
   });
 });
